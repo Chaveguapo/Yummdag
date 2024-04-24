0afec5dd330ac9669915167318e5e835c51ea8ab
app-house-listing <catakeas@gmail.com>
2024-04-24 12:21:10 +0200

    creation of recipe info

--- src/components/RecipeInformation.tsx
+++ src/components/RecipeInformation.tsx
@@ -1,23 +1,43 @@
-const RecipeInformation = () => {
-  function getRecipeInfo() {
-    fetch("https://api.spoonacular.com/recipes/{id}/information", {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        "x-Api-Key": "a055657f0b6e424b8a1b00e2fd26ebc0",
-      },
-    })
-      .then((response) => {
-        if (response.ok) return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        // Save the recipes to state
-        setRecipes(data);
-      })
-      .catch((error) => console.log("Error fetching data", error));
-  }
+function RecipeInformation() {
+  return (
+    <div>
+      <h1>Name of recipe</h1>
+      <img src="https://placehold.co/800x400" className="img-fluid" alt="" />
+      <div>
+        <div>
+          <span className="material-icons">schedule</span>
+          <p>Cooking time</p>
+        </div>
+        <div className="d-flex flex-wrap justify-content-center">
+          <span className="material-icons">favorite</span>
+          <span className="px-2">likes</span>
+        </div>
+      </div>
+      <div className="table-responsive">
+        <table className="table align-middle" />
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Ingredients</th>
+          </tr>
+        </thead>
 
-  return <div></div>;
-};
+        <tr>
+          <th scope="row">1</th>
+          <td>Ingredients 1</td>
+        </tr>
+      </div>
+      <h3>Let's start with...</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  );
+}
 export default RecipeInformation;
