876389726f44ceff7fcf2746e4b78ba176e7cdb8
app-house-listing <catakeas@gmail.com>
2024-04-30 11:14:13 +0200

    Not found page adjustments

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -2,14 +2,16 @@ import { Link, link } from "react-router-dom";
 
 export const NotFoundPage = () => {
   return (
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">
           Oops! It seems like you've stumbled upon a page that's taken a
           vacation
         </h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">404 Not Found</h6>
-        <p class="card-text">Let's get you back on track</p>
+        <h6 className="card-subtitle mb-2 text-body-secondary">
+          404 Not Found
+        </h6>
+        <p className="card-text">Let's get you back on track</p>
         <Link to="/"> Return to home and Happy browsing!</Link>
       </div>
     </div>
